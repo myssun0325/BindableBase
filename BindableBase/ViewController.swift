36642f1b24516013fa71828747e31d2b67829abc
mason <myssun0325@gmail.com>
2023-05-10 16:43:32 +0900

    init

---
+++ BindableBase/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BindableBase
+//
+//  Created by Mason on 2023/05/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
